bf1be7fe18ce273371d8fe0643fe4b36136c82a9
Stuart Wagner <stuwags@gmail.com>
2013-01-09 11:39:47 -0500

    Brand new app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CrawlTheBar::Application.config.secret_token = '6874362fb17c363a6812715d771518d3484ae38fd4e308cf5d5c8be67b00c020043972755c8a1fe61af821f605ec9f75991d5fe849433a27ce73edc160499e96'
